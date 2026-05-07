68bc76732eb982a1e749a5582bfa6a117da66c2c
micheatelier-maker <miche.atelier@gmail.com>
2026-05-06 17:44:46 -0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; import ErrorBoundary from './components/ErrorBoundary'; import './index.css'; const rootElement = document.getElementById('root'); if (!rootElement) { throw new Error("Could not find root element to mount to"); } const root = ReactDOM.createRoot(rootElement); root.render( <React.StrictMode> <ErrorBoundary> <App /> </ErrorBoundary> </React.StrictMode> );
